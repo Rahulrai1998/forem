2d88c4219faebedacbd501337678c9730465baec
RahulTechanzo <rahulrai2822march@gmail.com>
2026-01-24 15:26:11 +0530

    mikroorm setup

--- forem-server/src/index.ts
+++ forem-server/src/index.ts
@@ -1,3 +1,13 @@
 import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants.js";
 
-console.log("Hello  js coom ts");
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities:[], //corresponds to db-tables.
+    dbName: "forem",
+    debug: !__prod__ //checks the raw SQL-query running under the hood via orm.
+  });
+  console.log("Hellos coom ts");
+};
+
+main();
